1f3261f34709ea4f570c01f4e4d753106a3a743c
Jason Sexauer <genericcarbonlifeform@gmail.com>
2014-01-18 16:38:07 -0500

    Redis session testing

--- GreenButtonActuator.py
+++ GreenButtonActuator.py
@@ -5,57 +5,64 @@ Created on Fri Jan 17 19:34:01 2014
 @author: Jason
 """
 
-datafile = 'DailyElectricUsage'
-pnodes = ['BARBADOES35 KV  ABU2','BETZWOOD230 KV  LOAD1','PECO','_ENERGY_ONLY']
-weatherfile = 'KLOM_norristown'
+if __name__ == '__main__':
+    datafile = 'DailyElectricUsage'
+    pnodes = ['BARBADOES35 KV  ABU2',
+              'BETZWOOD230 KV  LOAD1',
+              'PECO',
+              '_ENERGY_ONLY']
+    weather_station = 'KLOM_norristown'
 
 
 import pandas
 import numpy as np
 import matplotlib.pylab as plt
 import os, sys
-plt.close('all')
 
 root = os.path.dirname(os.path.realpath(__file__))+os.sep
 
-# Read in usage log (csv format, probably specific to PECO)
-df = pandas.read_csv(root+datafile+'.csv', skiprows=4)
 
-# Convert costs (drop dollar sign and convert to float)
-df['COST'] = df['COST'].str.slice(1).apply(lambda x: float(x))
 
-# Convert times
 makeTimestamp = lambda x: pandas.Timestamp(x)
-df['ts'] = (df['DATE']+' '+df['START TIME']).apply(makeTimestamp)
-df.set_index('ts', drop=False, inplace=True)
 
-df['hr'] = df['ts'].apply(lambda x: int(x.strftime('%H')))
+def read_PECO_csv(datafile):
+    """Read csv file from PECO into a pandas dataframe"""
+    # Read in usage log (csv format, probably specific to PECO)
+    df = pandas.read_csv(root+datafile+'.csv', skiprows=4)
+    
+    # Convert costs (drop dollar sign and convert to float)
+    df['COST'] = df['COST'].str.slice(1).apply(lambda x: float(x))
+    
+    # Convert times
+    df['ts'] = (df['DATE']+' '+df['START TIME']).apply(makeTimestamp)
+    df.set_index('ts', drop=False, inplace=True)
+    
+    df['hr'] = df['ts'].apply(lambda x: int(x.strftime('%H')))
+    
+    
+    # Create a few tags
+    weekdayTagger = lambda x: 'Weekday' if x.weekday() else 'Weekend'
+    df['Weekday'] = df['ts'].apply(weekdayTagger)
+    df['DayOfWeek'] = df['ts'].apply(lambda x: x.strftime('%a'))
+    def getSeason(x):
+        month = x.month
+        if 6 <= month <= 8:
+            return 'Summer'
+        elif 9 <= month <= 11:
+            return 'Fall'
+        elif 3 <= month <= 5:
+            return 'Spring'
+        else:
+            return 'Winter'    
+    df['Season'] = df['ts'].apply(getSeason)
+    df['Month'] = df['ts'].apply(lambda x: x.strftime('%b'))
+    
+    assert len(df['UNITS'].unique()) == 1, "Energy units inconsistent"
+    
+    return df
 
 
-# Create a few tags
-df['Weekday'] = df['ts'].apply(lambda x: 'Weekday' if x.weekday() else 'Weekend')
-df['DayOfWeek'] = df['ts'].apply(lambda x: x.strftime('%a'))
-def getSeason(x):
-    month = x.month
-    if 6 <= month <= 8:
-        return 'Summer'
-    elif 9 <= month <= 11:
-        return 'Fall'
-    elif 3 <= month <= 5:
-        return 'Spring'
-    else:
-        return 'Winter'    
-df['Season'] = df['ts'].apply(getSeason)
-df['Month'] = df['ts'].apply(lambda x: x.strftime('%b'))
-
-
-# Look at only data since high-resolution started
-#df = df.ix['2013-11-20':]
-
-assert len(df['UNITS'].unique()) == 1, "Energy values in inconsistent units"
-
-
-def densityCloudByTags(df, columns):
+def density_cloud_by_tags(df, columns):
     """Create density cloud of data for a given tag or group of tags
     For example:
         columns='DayOfWeek' --> Plots for Mon, Tue, Wed, Thur, ...
@@ -111,40 +118,39 @@ def densityCloudByTags(df, columns):
         plt.grid(axis='y')
         plt.show()
 
-#densityCloudByTags(df, 'DayOfWeek')
-#densityCloudByTags(df, 'Weekday')
-#densityCloudByTags(df, ['Season','Weekday'])
-
 ############################################################################
 
 # What if we paid wholesale prices at our local pnode?
 
-for pnode in pnodes:
-    # Bring in PJM prices from DataMiner
-    pnode_prices = pandas.read_csv(root+'pnode_data/%s.csv' % pnode)
-    assert len(pnode_prices['PRICINGTYPE'].unique()) == 1
-    assert pnode_prices['PRICINGTYPE'].unique()[0] == 'TotalLMP'
+def price_at_pnodes(df, pnodes):
+    """ Given a green button dataframe, price that energy at PJM pnodes"""
+    for pnode in pnodes:
+        # Bring in PJM prices from DataMiner
+        pnode_prices = pandas.read_csv(root+'pnode_data/%s.csv' % pnode)
+        assert len(pnode_prices['PRICINGTYPE'].unique()) == 1
+        assert pnode_prices['PRICINGTYPE'].unique()[0] == 'TotalLMP'
+        
+        # Unpiviot the data
+        pnode_prices = pandas.melt(pnode_prices, id_vars=['PUBLISHDATE'],
+                    value_vars=['H%d'%i for i in xrange(1,25)])
+        pnode_prices = pnode_prices.rename(columns={
+                    'variable':'Hour',
+                    'value':'Price'})
+        # Convert hour to standard format and to hour beginning standard
+        cvtHr = lambda x: "%d:00" % (int(x)-1)
+        pnode_prices['Hour'] = pnode_prices['Hour'].str.slice(1).apply(cvtHr)
+        pnode_prices['ts'] = \
+            (pnode_prices['PUBLISHDATE']+' '+
+             pnode_prices['Hour'])              .apply(makeTimestamp)
+        pnode_prices = pnode_prices.set_index('ts', drop=False)
+        # Convert prices to $/kWhr (currently $/MWhr)
+        pnode_prices['Price'] = pnode_prices['Price']/1000
     
-    # Unpiviot the data
-    pnode_prices = pandas.melt(pnode_prices, id_vars=['PUBLISHDATE'],
-                value_vars=['H%d'%i for i in xrange(1,25)])
-    pnode_prices = pnode_prices.rename(columns={
-                'variable':'Hour',
-                'value':'Price'})
-    # Convert hour to standard format and to hour beginning standard
-    cvtHr = lambda x: "%d:00" % (int(x)-1)
-    pnode_prices['Hour'] = pnode_prices['Hour'].str.slice(1).apply(cvtHr)
-    pnode_prices['ts'] = \
-        (pnode_prices['PUBLISHDATE']+' '+
-         pnode_prices['Hour'])              .apply(makeTimestamp)
-    pnode_prices = pnode_prices.set_index('ts', drop=False)
-    # Convert prices to $/kWhr (currently $/MWhr)
-    pnode_prices['Price'] = pnode_prices['Price']/1000
-
-    # Figure out what our wholesale price would have been
-    df['pnode_'+pnode] = df['USAGE'] * pnode_prices['Price']
-
-cols = ['COST'] + ['pnode_'+p for p in pnodes]
+        # Figure out what our wholesale price would have been
+        df['pnode_'+pnode] = df['USAGE'] * pnode_prices['Price']
+    
+    return df
+#cols = ['COST'] + ['pnode_'+p for p in pnodes]
 #df[cols].plot()
 #df[cols].cumsum().plot()
 
@@ -152,89 +158,110 @@ cols = ['COST'] + ['pnode_'+p for p in pnodes]
 
 #############################################################################
 # Lets look at some weather correlations
-weather = pandas.read_csv(root+r'weather_data/%s.csv' % weatherfile,na_values=['N/A'])
-weather['ts'] = weather['DateUTC'].apply(makeTimestamp)
-weather = weather.set_index('ts', drop=False)
-weather['Wind SpeedMPH'] = weather['Wind SpeedMPH'].str.replace('Calm','0')
-weather['Wind SpeedMPH'] = weather['Wind SpeedMPH'].apply(lambda x: float(x))
-
-# Handle unclean data
-weather = weather.drop(weather[weather['TemperatureF'] < -20].index)
-weather = weather.drop(weather[weather['Wind SpeedMPH'] < 0].index)
-
-
-# Resample to hourly (taking average)
-weather2 = weather.resample('h')
-
-# Grab most frequest condition for hour
-condMode = lambda x: x.value_counts().index[0]
-makeTimestampKey = lambda x: makeTimestamp(x.strftime("%d-%b-%Y %H:00"))
-weather['ts_k'] = weather['ts'].apply(makeTimestampKey)
-weather2['Conditions'] = weather.groupby('ts_k')['Conditions'].apply(condMode)
-weather = weather2
+def load_weather(df, weather_station):
+    """Add weather tags to green button dataframe for a given station"""
+    weather = pandas.read_csv(root+
+        r'weather_data/%s.csv' % weather_station, na_values=['N/A'])
+    weather['ts'] = weather['DateUTC'].apply(makeTimestamp)
+    weather = weather.set_index('ts', drop=False)
+    weather['Wind SpeedMPH'] = weather['Wind SpeedMPH'].str.replace('Calm','0')
+    weather['Wind SpeedMPH'] = weather['Wind SpeedMPH'].apply(lambda x: float(x))
     
+    # Handle unclean data
+    weather = weather.drop(weather[weather['TemperatureF'] < -20].index)
+    weather = weather.drop(weather[weather['Wind SpeedMPH'] < 0].index)
+    
+    
+    # Resample to hourly (taking average)
+    weather2 = weather.resample('h')
+    
+    # Grab most frequest condition for hour
+    condMode = lambda x: x.value_counts().index[0]
+    makeTimestampKey = lambda x: makeTimestamp(x.strftime("%d-%b-%Y %H:00"))
+    weather['ts_k'] = weather['ts'].apply(makeTimestampKey)
+    weather2['Conditions'] = weather.groupby('ts_k')['Conditions'].apply(condMode)
+    weather = weather2
+        
+    
+    # Add some weather related tags
+    df['Temp'] = weather['TemperatureF']
+    df['Wind'] = weather['Wind SpeedMPH']
+    df['Conditions'] = weather['Conditions']
+    # 10 deg temp categories (ie, 50-60 deg)
+    tempGrads = lambda x: '%d-%d'% ((int(x/10)*10),(int(x/10)*10)+10)
+    df['TempGrads'] = df['Temp'].fillna(0).apply(tempGrads)
 
-# Add some weather related tags
-df['Temp'] = weather['TemperatureF']
-df['Wind'] = weather['Wind SpeedMPH']
-df['Conditions'] = weather['Conditions']
-# 10 deg temp categories (ie, 50-60 deg)
-tempGrads = lambda x: '%d-%d'% ((int(x/10)*10),(int(x/10)*10)+10)
-df['TempGrads'] = df['Temp'].fillna(0).apply(tempGrads)
+    return df
 
 
-#densityCloudByTags(df, 'TempGrads')
-#densityCloudByTags(df, 'Conditions')
 
 ############################################################################
 # Line Chart Test
 # API sandbox: https://code.google.com/apis/ajax/playground/?type=visualization#annotated_time_line
 # API docs: https://developers.google.com/chart/interactive/docs/gallery/annotatedtimeline?csw=1
 # DataTable docs: https://developers.google.com/chart/interactive/docs/reference?csw=1
-template = "        {c:[{v: 'Date(%(ts)s)'}, {v: %(USAGE)s}, {v: %(COST)s}]},\n"
-html = """
-<!--
-You are free to copy and use this sample in accordance with the terms of the
-Apache license (http://www.apache.org/licenses/LICENSE-2.0.html)
--->
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <title>Google Visualization API Sample</title>
-  <script type="text/javascript" src="http://www.google.com/jsapi"></script>
-  <script type="text/javascript">
-    google.load('visualization', '1', {packages: ['annotatedtimeline']});
-    function drawVisualization() {
-      var data = new google.visualization.DataTable(
-      {
-       cols: [{id: 'date', label: 'Date', type: 'datetime'},
-              {id: 'USAGE', label: 'Usage (kWh)', type: 'number'},
-              {id: 'COST', label: 'Cost ($)', type: 'number'}],
-       rows: [
-"""
-for lbl, r in df.iterrows():
-    r['ts']=r['ts'].strftime('%Y,%m,%d,%H,%M,%S')
-    html+= template % r.fillna('null')
-html +="""
-            ]
-      });
+def google_linechart(df):
+    template = "        {c:[{v: 'Date(%(ts)s)'}, {v: %(USAGE)s}, {v: %(COST)s}]},\n"
+    html = """
+    <!--
+    You are free to copy and use this sample in accordance with the terms of the
+    Apache license (http://www.apache.org/licenses/LICENSE-2.0.html)
+    -->
     
-      var annotatedtimeline = new google.visualization.AnnotatedTimeLine(
-          document.getElementById('visualization'));
-      annotatedtimeline.draw(data, {'displayAnnotations': true});
-    }
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <title>Google Visualization API Sample</title>
+      <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+      <script type="text/javascript">
+        google.load('visualization', '1', {packages: ['annotatedtimeline']});
+        function drawVisualization() {
+          var data = new google.visualization.DataTable(
+          {
+           cols: [{id: 'date', label: 'Date', type: 'datetime'},
+                  {id: 'USAGE', label: 'Usage (kWh)', type: 'number'},
+                  {id: 'COST', label: 'Cost ($)', type: 'number'}],
+           rows: [
+    """
+    for lbl, r in df.iterrows():
+        r['ts']=r['ts'].strftime('%Y,%m,%d,%H,%M,%S')
+        html+= template % r.fillna('null')
+    html +="""
+                ]
+          });
+        
+          var annotatedtimeline = new google.visualization.AnnotatedTimeLine(
+              document.getElementById('visualization'));
+          annotatedtimeline.draw(data, {'displayAnnotations': true});
+        }
+        
+        google.setOnLoadCallback(drawVisualization);
+      </script>
+    </head>
+    <body style="font-family: Arial;border: 0 none;">
+    <div id="visualization" style="width: 800px; height: 400px;"></div>
+    </body>
+    </html>
+    """
+    return html
+
+if __name__ == '__main__':
+    plt.close('all')
     
-    google.setOnLoadCallback(drawVisualization);
-  </script>
-</head>
-<body style="font-family: Arial;border: 0 none;">
-<div id="visualization" style="width: 800px; height: 400px;"></div>
-</body>
-</html>
-"""
-
-
+    # Load data
+    df = read_PECO_csv(datafile)
+    
+    # Add in the prices at nearby PJM pnodes
+    df = price_at_pnodes(df, pnodes)
+    density_cloud_by_tags(df, 'DayOfWeek')
+    density_cloud_by_tags(df, 'Weekday')
+    density_cloud_by_tags(df, ['Season','Weekday'])
+    
+    # Add in some weather info
+    df = load_weather(df, weather_station)
+    density_cloud_by_tags(df, 'TempGrads')
+    density_cloud_by_tags(df, 'Conditions')
+    
 
 
--- web/web.py
+++ web/web.py
@@ -2,26 +2,46 @@
 import imp, os, sys
 import traceback
 from flask import Flask, request, session
-
-root = root = os.path.dirname(os.path.realpath(__file__))+os.sep
-#GBA = imp.load_source('GBA', root+'..\GreenButtonActuator.py')
+from redis_session import RedisSessionInterface
 
 app = Flask(__name__)
 app.debug = True
+app.session_interface = RedisSessionInterface()
+
+root = root = os.path.dirname(os.path.realpath(__file__))+os.sep
+
+
+try:
+    GBA = imp.load_source('GBA', root+'..'+os.sep+'GreenButtonActuator.py')
+except:
+    debug_info = "<pre>"+str(traceback.format_exc())+"</pre>"
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def catch_all(path):
+        return 'PROBLEM IMPORTING GBA:<br>'+debug_info
+    
+    
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!<br>'
+    if 'df' in session.keys():
+        msg = 'You already have a dataframe loaded.  Good!<br><pre>%s</pre>' % session['df']
+        session.pop('df',None)
+        return msg
+    else:        
+        df = GBA.read_PECO_csv('DailyElectricUsage')
+        session['df'] = df
+        return 'Loaded daily electric usage.'
 
 @app.route('/raw')
 def viewraw():
-    #return 'wtf'
-    try:
-        GBA = imp.load_source('GBA', root+'..'+os.sep+'GreenButtonActuator.py')
-        return GBA.html
-    except:
-        return "ERROR:<pre>"+str(traceback.format_exc())+'</pre><br>'+root
+    return 'NotImplemented'
 
+
+
+app.secret_key = 'v\xfc\x9d\xfb\xa2\xc7uj\x97F\xc2\xb2\x14\xa4\xaa\xef\x8e\xedz\xe4\xc0daI'
 
 if __name__=='__main__':
     app.run()
